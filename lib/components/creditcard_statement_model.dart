82c81ce86161deaf87ff58d702e47f590816efe0
koki <deguguti@yahoo.co.jp>
2022-01-31 18:24:48 +0900

    モデルクラスの作成

---
+++ lib/components/creditcard_statement_model.dart
@@ -0,0 +1,26 @@
+class CreditcardStatement {
+  final int id;
+  final String cardName;
+  final int price;
+  final String note;
+
+  CreditcardStatement(
+      {required this.id,
+      required this.cardName,
+      required this.price,
+      required this.note});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'cardName': cardName,
+      'price': price,
+      'note': note,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'CreditcardStatement{id: $id, cardName: $cardName, price: $price, note: $note}';
+  }
+}
